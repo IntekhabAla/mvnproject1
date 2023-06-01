5fe06deb46c808cfad1c4063077aa8f0e2b65568
Atif <atif.mohammad109@gmail.com>
2023-06-02 00:31:52 +0530

    Added one Git_TestPush.java file

---
+++ src/test/java/com/sdetT/Git_TestPush.java
@@ -0,0 +1,12 @@
+package com.sdetT;
+
+public class Git_TestPush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Testing only push command in github.
+
+	}
+
+}
